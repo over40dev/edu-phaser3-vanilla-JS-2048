72d7d27e0fae391c6f46c7a547284bc820c621a9
over40dev <over40dev.com>
2020-01-19 22:13:10 -0330

    018-animating-tiles (w)

--- app/main.js
+++ app/main.js
@@ -1,13 +1,15 @@
 window.onload = function () {
-  const { cols, rows } = gameOptions.board;
-  const { tileSpacing, tileSize } = gameOptions.tiles;
-  const gameConfig = {
-    width: cols * (tileSize + tileSpacing) + tileSpacing,
-    height: rows * (tileSize + tileSpacing) + tileSpacing,
-    backgroundColor: 0xecf0f1,
-    scene: [BootGame, PlayGame]
-  }
+  const
+    { cols, rows, tileSpacing, tileSize } = gameOptions,
+    gameConfig = {
+      width: cols * (tileSize + tileSpacing) + tileSpacing,
+      height: rows * (tileSize + tileSpacing) + tileSpacing,
+      backgroundColor: 0xecf0f1,
+      scene: [BootGame, PlayGame]
+    };
+
   game = new Phaser.Game(gameConfig);
+  
   window.focus();
   CX.resize();
   window.addEventListener("resize", CX.resize);
