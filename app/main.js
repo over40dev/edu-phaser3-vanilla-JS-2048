7e3d3d5149fe1b3e7955982e4aa44f801726113a
over40dev <over40dev.com>
2020-01-28 18:03:38 -0330

    028- using Scale Manager

--- app/main.js
+++ app/main.js
@@ -1,17 +1,24 @@
-window.onload = function () {
+window.onload = onDeviceReady;
+document.addEventListener('deviceready', onDeviceReady);
+
+function onDeviceReady() {
   const
     { cols, tileSpacing, tileSize, aspectRatio } = gameOptions,
     width = cols * (tileSize + tileSpacing) + tileSpacing,
     gameConfig = { // based on Portrait layout
-      width: width,
-      height: width * aspectRatio,
+      scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        parent: 'game4096',
+        width: width,
+        height: width * aspectRatio,
+      },
       backgroundColor: 0x000000,
       scene: [BootGame, PlayGame],
+      type: Phaser.AUTO,
     };
 
   game = new Phaser.Game(gameConfig);
   
   window.focus();
-  CX.resize();
-  window.addEventListener("resize", CX.resize);
 }
