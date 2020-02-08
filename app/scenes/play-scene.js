908009c1692b43002670853d660b46d4ef71c7e4
over40dev <over40dev.com>
2020-02-07 23:44:48 -0330

    030 - class to extract logic & game  engine dep

--- app/scenes/play-scene.js
+++ app/scenes/play-scene.js
@@ -9,32 +9,37 @@ class PlayGame extends Phaser.Scene {
 
   create() {
     const
-      { COVER_TILE_VAL } = gameConstants,
-      { rows, cols, localStorageName } = gameOptions,
+      { rows, cols, localStorageName, max2048: maxValue } = gameOptions;
+    
+      this.game4096 = new Game4096({
+      rows,
+      cols,
+      maxValue,
+    });
+
+    const
       restartXY = this.getTilePosition(-0.8, cols - 1),
       restartBtn = this.add.sprite(restartXY.x, restartXY.y, 'restart'),
       fullScreenButton = this.add.sprite(restartBtn.x, restartBtn.y - 120, 'fullscreen'),
-      scoreXY = this.getTilePosition(-0.8, 1),
-      scorePanel = this.add.image(scoreXY.x, scoreXY.y, 'scorepanel'),
-      scoreLabels = this.add.image(scoreXY.x, scoreXY.y - 70, 'scorelabels'),
       gameTitle = this.add.image(10, 5, 'gametitle'),
       howTo = this.add.image(game.config.width, 5, 'howtoplay'),
       logo = this.add.sprite(game.config.width / 2, game.config.height, 'logo');
     let
+      scoreXY,
       textXY;
 
+    scoreXY = this.getTilePosition(-0.8, 1);
+    this.add.image(scoreXY.x, scoreXY.y, 'scorepanel');
+    this.add.image(scoreXY.x, scoreXY.y - 70, 'scorelabels');
 
     textXY = this.getTilePosition(-.92, -.4);
     this.scoreText = this.add.bitmapText(textXY.x, textXY.y, 'font', 0);
 
     textXY = this.getTilePosition(-.92, 1.1);
-    this.bestScore = localStorage.getItem(localStorageName);
-    if (this.bestScore === null) {
-      this.bestScore = 0;
-    }
+    this.bestScore = localStorage.getItem(localStorageName) || 0;
     this.bestScoreText = this.add.bitmapText(textXY.x, textXY.y, 'font', this.bestScore.toString());
 
-    this.score = 0
+    this.score = 0;
 
     gameTitle.setOrigin(0, 0);
     howTo.setOrigin(1, 0);
@@ -63,20 +68,15 @@ class PlayGame extends Phaser.Scene {
       });
 
     this.canMove = false;
-    this.boardArray = [];
+    this.game4096.generateGameBoard();
 
-    for (let row = 0; row < rows; row++) {
-      this.boardArray[row] = [];
-      for (let col = 0; col < cols; col++) {
+    for (let row = 0; row < this.game4096.getRows(); row++) {
+      for (let col = 0; col < this.game4096.getCols(); col++) {
         const { x, y } = this.getTilePosition(row, col);
         this.add.image(x, y, 'emptytile');
         const tileSprite = this.add.sprite(x, y, 'tiles', 0);
         tileSprite.visible = false;
-        this.boardArray[row][col] = {
-          value: COVER_TILE_VAL,
-          sprite: tileSprite,
-          upgraded: false,
-        };
+        this.game4096.setCustomData(row, col, tileSprite);
       }
     }
     this.addTile();
@@ -89,221 +89,132 @@ class PlayGame extends Phaser.Scene {
 
   addTile() {
     const
-      emptyTiles = [],
-      { cols, rows, tweenSpeed } = gameOptions;
+      { tweenSpeed } = gameOptions,
+      { row, col } = this.game4096.addTile(),
+      tileSprite = this.game4096.getCustomData(row, col);
 
-    for (let row = 0; row < rows; row++) {
-      for (let col = 0; col < cols; col++) {
-        if (this.isEmptyCell(row, col)) {
-          emptyTiles.push({ row, col });
-        }
-      }
-    }
-    if (emptyTiles.length > 0) {
-      const
-        { row, col } = CX.getRandomElement(emptyTiles),
-        chosenTile = this.boardArray[row][col];
-
-      if (!!chosenTile) {
-        chosenTile.value = 1;
-        chosenTile.sprite.visible = true;
-        chosenTile.sprite.setFrame(0);
-        chosenTile.sprite.alpha = 0;
-        this.tweens.add({
-          targets: [chosenTile.sprite],
-          alpha: 1,
-          duration: tweenSpeed,
-          callbackScope: this,
-          onComplete: () => { this.canMove = true },
-        });
-      }
-    }
+    tileSprite.visible = true;
+    tileSprite.setFrame(0);
+    tileSprite.alpha = 0;
+    this.tweens.add({
+      targets: tileSprite,
+      alpha: 1,
+      duration: tweenSpeed,
+      callbackScope: this,
+      onComplete: () => { this.canMove = true },
+    });
   }
 
-  isEmptyCell(row, col) {
+  getTilePosition(row, col) {
     const
-      { COVER_TILE_VAL } = gameConstants;
+      { rows, tileSize, tileSpacing } = gameOptions;
+    let
+      posX = (col + 1) * tileSpacing + (col + .5) * tileSize,
+      posY = (row + 1) * tileSpacing + (row + .5) * tileSize,
+      offsetY = 0,
+      boardHeight = 0;
 
-    return (this.boardArray[row][col].value === COVER_TILE_VAL);
+    boardHeight += this.game4096.getRows() * tileSize;
+    boardHeight += (this.game4096.getRows() + 1) * tileSpacing;
+    offsetY += (game.config.height - boardHeight) / 2;
+    posY += offsetY;
+
+    return CX.getVectorPoint(posX, posY);
   }
 
   handleKey(e) {
-    if (this.canMove) {
-      // look for WASD and arrow keys to move using keyboard
-      const
-        { LEFT, RIGHT, UP, DOWN } = gameConstants;
+    if (!this.canMove) return;
+    // look for WASD and arrow keys to move using keyboard
 
-      switch (e.code) {
-        case "ArrowLeft":
-        case "KeyA":
-          this.makeMove(LEFT);
-          break;
-        case "ArrowRight":
-        case "KeyD":
-          this.makeMove(RIGHT);
-          break;
-        case "ArrowUp":
-        case "KeyW":
-          this.makeMove(UP);
-          break;
-        case "ArrowDown":
-        case "KeyS":
-          this.makeMove(DOWN);
-          break;
+    switch (e.code) {
+      case "ArrowLeft":
+      case "KeyA":
+        this.makeMove(this.game4096.LEFT);
+        break;
+      case "ArrowRight":
+      case "KeyD":
+        this.makeMove(this.game4096.RIGHT);
+        break;
+      case "ArrowUp":
+      case "KeyW":
+        this.makeMove(this.game4096.UP);
+        break;
+      case "ArrowDown":
+      case "KeyS":
+        this.makeMove(this.game4096.DOWN);
+        break;
 
-        default:
-          break;
-      }
+      default:
+        break;
     }
   }
 
   handleSwipe(e) {
-    if (this.canMove) {
-      const
-        // { swipeMinNormal } = gameOptions,
-        swipe = CX.getVectorPoint(e.upX - e.downX, e.upY - e.downY);
+    if (this.canMove) return;
 
-      if (this.validSwipe(e, swipe)) {
-        this.handleMove(swipe);
-      }
+    const
+      // { swipeMinNormal } = gameOptions,
+      swipe = CX.getVectorPoint(e.upX - e.downX, e.upY - e.downY);
+
+    if (this.validSwipe(e, swipe)) {
+      this.handleMove(swipe);
     }
   }
 
   validSwipe(e, swipe) {
+    if (!e || !swipe) return false;
+
     const
       { swipeMaxTime, swipeMinDist } = gameOptions,
       swipeTime = e.upTime - e.downTime,
-      swipeMagnitude = CX.getMagnitude(swipe),
       fastEnough = swipeTime < swipeMaxTime,
+      swipeMagnitude = CX.getMagnitude(swipe),
       longEnough = swipeMagnitude > swipeMinDist;
 
     return longEnough && fastEnough;
   }
 
   handleMove(swipe) {
-    if (!swipe) { return; }
+    if (!swipe) return;
 
     const
-      { swipeMinNormal } = gameOptions,
-      { RIGHT, LEFT, UP, DOWN } = gameConstants;
+      { swipeMinNormal } = gameOptions;
 
-    // CX.setMagnitude(swipe, 1), // normalize vector to magnitude of 1
-    Phaser.Geom.Point.SetMagnitude(swipe, 1);
+    CX.setMagnitude(swipe, 1); // normalize vector to magnitude of 1
     if (swipe.x > swipeMinNormal) {
-      this.makeMove(RIGHT);
+      this.makeMove(this.game4096.RIGHT);
     }
     if (swipe.x < -swipeMinNormal) {
-      this.makeMove(LEFT);
+      this.makeMove(this.game4096.LEFT);
     }
     if (swipe.y > swipeMinNormal) {
-      this.makeMove(DOWN);
+      this.makeMove(this.game4096.DOWN);
     }
     if (swipe.y < -swipeMinNormal) {
-      this.makeMove(UP);
+      this.makeMove(this.game4096.UP);
     }
   }
 
-  /** TODO: makeMove impl v04 */
-  makeMove(d = null) {
-    if (d) {
-      this.movingTiles = 0;
+  makeMove(direction = null) {
+    if (!direction) return;
+
+    const movements = this.game4096.moveBoard(direction);
+
+    if (movements.length > 0) {
       this.canMove = false;
-    } else {
-      return null;
-    }
-
-    const
-      { COVER_TILE_VAL } = gameConstants,
-      { dRow, dCol, firstRow, lastRow, firstCol, lastCol, }
-        = this.setMoveOptions(d);
-
-    for (let row = firstRow; row < lastRow; row++) {
-      for (let col = firstCol; col < lastCol; col++) {
-        const
-          curRow = dRow === 1 ? (lastRow - 1) - row : row,
-          curCol = dCol === 1 ? (lastCol - 1) - col : col,
-          curTile = this.boardArray[curRow][curCol],
-          tileValue = curTile.value;
-
-        if (tileValue !== 0) {
-          let newRow = curRow;
-          let newCol = curCol;
-          while (this.isLegalPosition(newRow + dRow, newCol + dCol, tileValue)) {
-            newRow += dRow;
-            newCol += dCol;
-          }
-
-          if (newRow !== curRow || newCol !== curCol) {
-            const
-              newTile = this.boardArray[newRow][newCol],
-              newPos = this.getTilePosition(newRow, newCol),
-              willUpdate = newTile.value === tileValue;
-
-            this.moveTile(curTile.sprite, newPos, willUpdate);
-            this.updateTilePosition(curTile, newTile);
-            curTile.value = COVER_TILE_VAL;
-            if (willUpdate) {
-              newTile.value++;
-              newTile.upgraded = true;
-              this.score += Math.pow(2, newTile.value);
-            } else {
-              newTile.value = tileValue;
-            }
-          }
-        }
-      }
-    }
-    if (this.movingTiles === 0) {
-      this.canMove = true;
-    } else {
+      this.movingTiles = 0;
       this.moveSound.play();
-    }
-  }
-
-  setMoveOptions(d) {
-    const
-      { LEFT, RIGHT, UP, DOWN } = gameConstants,
-      { rows, cols } = gameOptions;
-
-    return {
-      dRow: (d === LEFT || d === RIGHT) ? 0 : (d === UP) ? -1 : 1,
-      dCol: (d === UP || d === DOWN) ? 0 : (d === LEFT) ? -1 : 1,
-      firstRow: (d === UP) ? 1 : 0,
-      lastRow: rows - ((d === DOWN) ? 1 : 0),
-      firstCol: (d === LEFT) ? 1 : 0,
-      lastCol: cols - ((d === RIGHT) ? 1 : 0),
-    }
-  }
-
-  refreshBoard() {
-    const
-      { rows, cols, localStorageName } = gameOptions;
-
-    this.scoreText.text = this.score.toString();
-    if (this.score > this.bestScore) {
-      this.bestScore = this.score;
-      localStorage.setItem(localStorageName, this.bestScore);
-      this.bestScoreText.text = this.bestScore.toString();
-    }
-
-    for (let row = 0; row < rows; row++) {
-      for (let col = 0; col < cols; col++) {
+      movements.forEach(move => {
         const
-          { x, y } = this.getTilePosition(row, col),
-          tile = this.boardArray[row][col];
+          newPos = this.getTilePosition(move.to.row, move.to.col),
+          shouldUpgrade = move.from.value !== move.to.value;
 
-        tile.sprite.x = x;
-        tile.sprite.y = y;
-        if (tile.value > 0) {
-          tile.sprite.visible = true;
-          tile.sprite.setFrame(tile.value - 1);
-          tile.upgraded = false;
-        } else {
-          tile.sprite.visible = false;
+        this.moveTile(this.game4096.getCustomData(move.from.row, move.from.col), newPos, shouldUpgrade);
+        if (shouldUpgrade) {
+          this.score += Math.pow(2, move.to.value);
         }
-      }
+      });
     }
-    this.addTile();
   }
 
   moveTile(tileSprite, newPos, upgrade) {
@@ -330,11 +241,10 @@ class PlayGame extends Phaser.Scene {
   }
 
   upgradeTile(tileSprite) {
-    const
-      { tweenSpeed } = gameOptions;
+    const { tweenSpeed } = gameOptions;
 
-    tileSprite.setFrame(tileSprite.frame.name + 1);
     this.growSound.play();
+    tileSprite.setFrame(tileSprite.frame.name + 1);
     this.tweens.add({
       targets: [tileSprite],
       scaleX: 1.1,
@@ -343,7 +253,7 @@ class PlayGame extends Phaser.Scene {
       yoyo: true,
       repeat: 1,
       callbackScope: this,
-      onComplete: function () {
+      onComplete() {
         this.endTween(tileSprite);
       }
     });
@@ -357,58 +267,34 @@ class PlayGame extends Phaser.Scene {
     }
   }
 
-  updateTilePosition(curTile, newTile) {
-    if (newTile.value === curTile.value) {
-      newTile.value++;
-      newTile.upgraded = true;
-      curTile.sprite.setFrame(curTile.value);
-    } else {
-      newTile.value = curTile.value;
-    }
-  }
-
-  getTilePosition(row, col) {
+  refreshBoard() {
     const
-      { rows, cols, tileSize, tileSpacing } = gameOptions;
-    let
-      posX = (col + 1) * tileSpacing + (col + .5) * tileSize,
-      posY = (row + 1) * tileSpacing + (row + .5) * tileSize,
-      boardHeight = (rows + 1) * tileSpacing + (rows * tileSize),
-      offsetY = (game.config.height - boardHeight) / 2;
+      { localStorageName } = gameOptions;
 
-    posY += offsetY;
-
-    // return CX.getVectorPoint(posX, posY);
-    return new Phaser.Geom.Point(posX, posY);
-  }
-
-  /** @summary check if legal move by verifying position
-   * @param {number} row 
-   * @param {number} col 
-   * @param {number} value 
-   */
-  isLegalPosition(row, col, value) {
-    const
-      { rows, cols } = gameOptions,
-      { COVER_TILE_VAL } = gameConstants,
-      rowInside = row >= 0 && row < rows,
-      colInside = col >= 0 && col < cols;
-
-    if (!rowInside || !colInside) {
-      return false;
+    this.scoreText.text = this.score.toString();
+    if (this.score > this.bestScore) {
+      this.bestScore = this.score;
+      localStorage.setItem(localStorageName, this.bestScore);
+      this.bestScoreText.text = this.bestScore.toString();
     }
 
-    if (this.boardArray[row][col].value === 12) {
-      return false;
+    for (let row = 0; row < this.game4096.getRows(); row++) {
+      for (let col = 0; col < this.game4096.getCols(); col++) {
+        const
+          { x, y } = this.getTilePosition(row, col),
+          tileValue = this.game4096.getTileValue(row, col);
+
+        this.game4096.getCustomData(row, col).x = x;
+        this.game4096.getCustomData(row, col).y = y;
+
+        if (tileValue > 0) {
+          this.game4096.getCustomData(row, col).visible = true;
+          this.game4096.getCustomData(row, col).setFrame(tileValue - 1);
+        } else {
+          this.game4096.getCustomData(row, col).visible = false;
+        }
+      }
     }
-
-    const
-      tile = this.boardArray[row][col],
-      emptySpot = tile.value === COVER_TILE_VAL,
-      sameValue = tile.value === value,
-      alreadyUpgraded = tile.upgraded;
-
-    // 2 legal positions to move --- empty && same tile value
-    return (emptySpot || (sameValue && !alreadyUpgraded));
+    this.addTile();
   }
 }
